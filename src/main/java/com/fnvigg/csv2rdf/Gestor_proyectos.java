98531a819f453fb451b68b9a745f4bb6cf1dda3a
Wolfax <61865323+FrancoNicolaViggianoURJC@users.noreply.github.com>
2024-01-03 02:47:49 +0100

    Escena Proyectos
    Ver postit

--- src/main/java/com/fnvigg/csv2rdf/Gestor_proyectos.java
+++ src/main/java/com/fnvigg/csv2rdf/Gestor_proyectos.java
@@ -1,6 +1,8 @@
 package com.fnvigg.csv2rdf;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,6 +22,7 @@ public class Gestor_proyectos {
         File[] listado = directorio.listFiles();
         if(listado != null){
             for(File fichero : listado){
+                fichero.listFiles();
                 filenames.add(fichero.getName());
             }
         }else{
@@ -31,4 +34,41 @@ public class Gestor_proyectos {
         return filenames;
     }
 
+    public void borrarProyecto(String proyecto) {
+        File dir = new File("src/main/resources/Proyectos/"+ proyecto);
+        if (dir!=null){
+            //Para borrar el directorio, hay que borrar recursivamente los ficheros contenidos
+            File[] ficheros = dir.listFiles();
+            if(ficheros != null){
+                for(File file : ficheros){
+                    Boolean report = file.delete();
+                }
+            }
+            //Una vez vacio, lo borramos
+            dir.delete();
+        }else{
+            System.out.println("error");
+        }
+
+    }
+
+    public void crearProyecto(String nombreProyecto, String nombreOntologico, String nombreDato) {
+        //Creacion de directorio
+        File dir = new File("src/main/resources/Proyectos/"+nombreProyecto);
+        dir.mkdir();
+        //Creacion del fichero de configuracion
+        File config = new File("src/main/resources/Proyectos/"+nombreProyecto+"/"+"config.txt");
+        try {
+            config.createNewFile();
+            FileWriter fr = new FileWriter(config, true);
+            BufferedWriter br = new BufferedWriter(fr);
+            br.write(nombreOntologico + "," + nombreDato);
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/com/fnvigg/csv2rdf/ProyectosController.java
+++ src/main/java/com/fnvigg/csv2rdf/ProyectosController.java
@@ -7,18 +7,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ProyectosController implements Initializable {
+    public Label lblDatos;
+    public Label lblOntologico;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -50,5 +51,102 @@ public class ProyectosController implements Initializable {
 
     }
 
-    
+    @FXML
+    private boolean mostrarAlerta(ActionEvent event){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Confirmacion de acción");
+        alerta.setContentText("¿Estas seguro de querer borrar el proyecto?");
+        Optional<ButtonType> resultado = alerta.showAndWait();
+
+        if(resultado.isEmpty()){
+            System.out.println("Cerrada");
+            return false;
+        }else if(resultado.get() == ButtonType.OK){
+            System.out.println("Borrar proyecto");
+            return true;
+        }else if(resultado.get() == ButtonType.CANCEL){
+            System.out.println("Cancelado");
+            return false;
+        }
+        return false;
+    }
+
+    private void actualizarLista(){
+        try {
+            List<String> listaProyectos = proyectos.getProyectos();
+            ObservableList<String> listaObservable = FXCollections.observableArrayList();
+            listaObservable.addAll(listaProyectos);
+            //Popular el listview
+            listProyects.setItems(listaObservable);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void onClickCargarProyecto(ActionEvent event) {
+
+    }
+
+    public void onClickBorrarProyecto(ActionEvent event) {
+        if(mostrarAlerta(event)) {
+            String proyecto = (String) listProyects.getSelectionModel().getSelectedItem();
+            proyectos.borrarProyecto(proyecto);
+            actualizarLista();
+            lblDatos.setVisible(false);
+            lblOntologico.setVisible(false);
+        }else{
+
+        }
+    }
+
+    public void onClickRegistrarProyecto(ActionEvent event) {
+
+        String nombreProyecto = nameProyectFld.getText();
+        String nombreOntologico = ontologicProyectFld.getText();
+        String nombreDato = dataProyectFld.getText();
+        boolean nombreDisponible = true;
+        for (Object proyecto : listProyects.getItems()){
+            String proyectoString = (String) proyecto;
+            if(nombreProyecto.equals(proyectoString)){
+                nombreDisponible = false;
+                break;
+            }
+        }
+        if(nombreDisponible) {
+            proyectos.crearProyecto(nombreProyecto, nombreDato, nombreOntologico);
+            actualizarLista();
+            nameProyectFld.setText("");
+            ontologicProyectFld.setText("");
+            dataProyectFld.setText("");
+        }else{
+            //mostrar mensaje error...
+        }
+    }
+
+    public void onClickDatosProyecto(MouseEvent mouseEvent) {
+        //Recoger los nombres del ing datos, ontologico y cambiar las properties de las label de abajo
+        String proyecto = (String) listProyects.getSelectionModel().getSelectedItem();
+        File fichero = new File("src/main/resources/Proyectos/" + proyecto + "/config.txt");
+        try {
+            FileReader fr = new FileReader(fichero);
+            BufferedReader br = new BufferedReader(fr);
+            String linea = br.readLine();
+            String ingDato, ingOnt;
+            String[] tokens = linea.split(",");
+            ingDato = tokens[0];
+            ingOnt = tokens[1];
+
+            lblDatos.setText(ingDato);
+            lblOntologico.setText(ingOnt);
+            lblDatos.setVisible(true);
+            lblOntologico.setVisible(true);
+
+            fr.close();
+            br.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
